adb79755618f1137e5074b41cd34bbc31e4c4453
maja345eee <121087018+maja345eee@users.noreply.github.com>
2025-01-09 13:27:21 +0100

    yayayayayay

--- Assets/Scripts/Maja scripts/Dialogue.cs
+++ Assets/Scripts/Maja scripts/Dialogue.cs
@@ -1,8 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    
+    //från videon "5 Minute DIALOGUE SYSTEM in UNITY Tutorial" av BMo
+
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    private void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        //Type each character 1 by 1
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    private void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
