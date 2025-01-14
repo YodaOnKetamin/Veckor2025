822bf7cffabedb27fce7160283dac1f791ec4344
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2025-01-14 08:44:22 +0100

    ja

---
+++ Assets/Scripts/Maja scripts/doneDialogue.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class doneDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    ControllerForPatienter PC;
+    GameObject Controler;
+
+    private void Start()
+    {
+        Controler = GameObject.FindWithTag("Controller");
+        PC = Controler.GetComponent<ControllerForPatienter>();
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        //Type each character 1 by 1
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    private void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            PC.klarmedpatient = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
