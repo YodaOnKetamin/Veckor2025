7cbf1e7500804adfb2182938f95b369680f8f341
maja345eee <121087018+maja345eee@users.noreply.github.com>
2025-01-15 09:49:55 +0100

    files

---
+++ Assets/Scripts/Maja scripts/files.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class files : MonoBehaviour
+{
+    public ControllerForPatienter simonScriptYAY;
+
+    public GameObject adamFilesYAY;
+    public GameObject fishFilesYAY;
+    public GameObject seaAirFilesYAY;
+    public GameObject govAgentFilesYAY;
+    public GameObject TRETTIyrsFilesYAY;
+    public GameObject workWomanFilesYAY;
+    public GameObject priestFilesYAY;
+    public GameObject artistFilesYAY;
+    public GameObject whereAmIFilesYAY;
+
+
+    public void filesYAY()
+    {
+        if(simonScriptYAY.activePatient == 1)
+        {
+            adamFilesYAY.SetActive(true);
+        }
+        if(simonScriptYAY.activePatient == 2)
+        {
+            fishFilesYAY.SetActive(true);
+            adamFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 3)
+        {
+            seaAirFilesYAY.SetActive(true);
+            fishFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 4)
+        {
+            govAgentFilesYAY.SetActive(true);
+            seaAirFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 5)
+        {
+            TRETTIyrsFilesYAY.SetActive(true);
+            govAgentFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 6)
+        {
+            workWomanFilesYAY.SetActive(true);
+            TRETTIyrsFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 7)
+        {
+            priestFilesYAY.SetActive(true);
+            workWomanFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 8)
+        {
+            artistFilesYAY.SetActive(true);
+            priestFilesYAY.SetActive(false);
+        }
+        if(simonScriptYAY.activePatient == 9)
+        {
+            whereAmIFilesYAY.SetActive(true);
+            artistFilesYAY.SetActive(false);
+        }
+    }
+}
