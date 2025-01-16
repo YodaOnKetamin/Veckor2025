6dd2e7544c1d7cf8022b41bdd8565d71cbe391f4
lillamyy <114159749+lillamyy@users.noreply.github.com>
2025-01-16 11:03:27 +0100

    Revert "snälla funka"
    This reverts commit 9699505c0e30dba58b38e311a05fe319bd38e84c.

--- Assets/Scripts/Maja scripts/Dialogue.cs
+++ Assets/Scripts/Maja scripts/Dialogue.cs
@@ -11,24 +11,10 @@ public class Dialogue : MonoBehaviour
 
     private int index;
 
-    [SerializeField] private AudioClip[] dialogueTypingSoundClips;
-    [Range(1, 5)]
-    [SerializeField] private int frequencyLevel = 2;
-    [Range(-3, 3)]
-    [SerializeField] private float minPitch = 0.5f;
-    [Range(-3, 3)]
-    [SerializeField] private float maxPitch = 3f;
-
-    [SerializeField] private bool stopAudioSource;
-
-    private AudioSource audioSource;
-
     private void Start()
     {
         textComponent.text = string.Empty;
         StartDialogue();
-
-        audioSource = this.gameObject.AddComponent<AudioSource>();
     }
 
     private void Update()
@@ -59,30 +45,7 @@ public class Dialogue : MonoBehaviour
         foreach (char c in lines[index].ToCharArray())
         {
             textComponent.text += c;
-
-            PlayDialogueSound(c);
-
             yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    private void PlayDialogueSound(int currentDisplayedCharacterCount)
-    {
-        //kollar om antalet bokstäver är delbart med det man har ställt frequencyLevel till, ifall det är sant spelas ljudet tex varanan bokstav bokstav 
-        //frequencyLevel ändras med slider 
-        if (currentDisplayedCharacterCount % frequencyLevel == 0)
-        {
-            if (stopAudioSource)
-            {
-                audioSource.Stop();
-            }
-            //spelar clip 
-            int randomIndex = Random.Range(0, dialogueTypingSoundClips.Length);
-            AudioClip soundClip = dialogueTypingSoundClips[randomIndex];
-            //pitch
-            audioSource.pitch = Random.Range(minPitch, maxPitch);
-            // spelar ljud
-            audioSource.PlayOneShot(soundClip);
         }
     }
 
