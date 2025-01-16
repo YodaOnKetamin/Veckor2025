b6d1aeab78ce4e4df1fb6433ba4bd1fd64787850
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2025-01-16 10:48:49 +0100

    new patients in scene

---
+++ Assets/Scripts/Simon/EndingManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingManager : MonoBehaviour
+{
+    int Warnings;
+    int lightTreatemnts;
+    int Sanity;//5 - 0
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
