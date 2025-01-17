cd07e8c1146f36821966abc4f28ee08754d2515c
Kungozz <ossian.lie@icloud.com>
2025-01-17 09:12:48 +0100

    Lagt till gameover scene

--- Assets/Scripts/Ossian Scripts/Ingamemenuscript.cs
+++ Assets/Scripts/Ossian Scripts/Ingamemenuscript.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ingamemenuscript : MonoBehaviour
 {
     [SerializeField]
     GameObject pausemenu;
-    
+    [SerializeField]
+    GameObject GameOverMenu;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -22,6 +24,10 @@ public class Ingamemenuscript : MonoBehaviour
     {
         //måste fixa så att spelet startar igen och allt skit med det! :)
         togglePanel(pausemenu);
+    }
+    public void GameOver()
+    {
+        togglePanel(GameOverMenu);
     }
     private void togglePanel(GameObject panel)
     {
